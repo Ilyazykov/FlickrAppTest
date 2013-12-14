c637cf650d6070a19174fb9f759a8488ec10c8a9
Ilyazykov <ilya.zykov@icloud.com>
2013-12-14 23:38:42 +0400

    Проект Flickr с mvvm паттерном. Создал model

---
+++ FlickrMVVM/FlickrMVVM/Model/Photo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlickrMVVM.Model
+{
+    public class Photo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string SmallUrl { get; set; }
+        public string LargeUrl { get; set; }
+
+        public Photo() { }
+
+        public Photo(int id, string title, string smallUrl, string largeUrl)
+        {
+            ID = id;
+            Title = title;
+            SmallUrl = smallUrl;
+            LargeUrl = largeUrl;
+        }
+    }
+}
