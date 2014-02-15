32969aec23858b8f44432865715169f5fec474f5
Ilyazykov <ilya.zykov@icloud.com>
2014-02-15 07:00:27 +0400

    IsoImageConverter added

---
+++ FlickrPhoneApp/FlickrPhoneApp/IsoImageConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FlickrPhoneApp
+{
+    class IsoImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            var bitmap = new BitmapImage();
+            try
+            {
+                var path = (string)value;
+                if (!String.IsNullOrEmpty(path))
+                {
+                    using (var file = LoadFile(path))
+                    {
+                        bitmap.SetSource(file);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return bitmap;
+        }
+
+        private Stream LoadFile(string file)
+        {
+            using (var isoStore = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                return isoStore.OpenFile(file, FileMode.Open, FileAccess.Read);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
