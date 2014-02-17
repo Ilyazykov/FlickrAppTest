29e1a5ff5d5181efb10ce1f820d6a5d3889b111c
Ilyazykov <ilya.zykov@icloud.com>
2014-02-17 05:37:01 +0400

    Изображения отображаются и кешируются при наличии интернета

--- FlickrPhoneApp/FlickrPhoneApp/IsoImageConverter.cs
+++ FlickrPhoneApp/FlickrPhoneApp/IsoImageConverter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
@@ -12,26 +14,107 @@ namespace FlickrPhoneApp
     public class IsoImageConverter : IValueConverter
     {
         private static readonly IsolatedStorageFile Storage = IsolatedStorageFile.GetUserStoreForApplication();
+        
+        public static string StorageFolder = "ImageCache";
+
 
         public object Convert(object value, Type targetType, object parameter,
         System.Globalization.CultureInfo culture)
         {
-            var bitmap = new BitmapImage();
-            try
+            var path = value as string;
+            if (String.IsNullOrEmpty(path)) return null;
+
+            var imageFileUri = new Uri(path);
+            if (imageFileUri.Scheme == "http" || imageFileUri.Scheme == "https")
             {
-                var path = (string)value;
-                if (!String.IsNullOrEmpty(path))
+                if (!NetworkInterface.GetIsNetworkAvailable())
                 {
-                    using (var file = LoadFile(path))
+                    if (Storage.FileExists(GetFileNameInIsolatedStorage(imageFileUri)))
+                    {
+                        return ExtractFromLocalStorage(imageFileUri);
+                    }
+                    return LoadDefaultIfPassed(imageFileUri, (parameter ?? string.Empty).ToString());
+                }
+                return DownloadFromWeb(imageFileUri);
+            }
+
+            var bitmap = new BitmapImage(imageFileUri);
+            return bitmap;
+        }
+
+        private static object DownloadFromWeb(Uri imageFileUri)
+        {
+            var webClient = new WebClient();
+
+            var bitmap = new BitmapImage();
+
+            webClient.OpenReadCompleted += (o, e) =>
+            {
+                if (e.Error != null || e.Cancelled) return;
+
+                WriteToIsolatedStorage(IsolatedStorageFile.GetUserStoreForApplication(), e.Result,
+                    GetFileNameInIsolatedStorage(imageFileUri));
+
+                try
+                {
+                    string isolatedStoragePath = GetFileNameInIsolatedStorage(imageFileUri);
+                    using (var file = Storage.OpenFile(isolatedStoragePath, FileMode.Open, FileAccess.Read))
                     {
                         bitmap.SetSource(file);
                     }
                 }
+                catch (Exception)
+                {
+                }
+
+                e.Result.Close();
+            };
+            webClient.OpenReadAsync(imageFileUri);
+            return bitmap;
+        }
+
+        private static string GetFileNameInIsolatedStorage(Uri imageFileUri)
+        {
+            return StorageFolder + "\\" + imageFileUri.AbsoluteUri.GetHashCode() + ".img";
+        }
+
+        private static void WriteToIsolatedStorage(IsolatedStorageFile file, Stream stream, string fileName)
+        {
+            IsolatedStorageFileStream outputStream = null;
+            try
+            {
+                if (!file.DirectoryExists(StorageFolder));
+                {
+                    file.CreateDirectory(StorageFolder);
+                }
+                if (file.FileExists(fileName))
+                {
+                    file.DeleteFile(fileName);
+                }
+                outputStream = file.CreateFile(fileName);
+
+                var buffer = new byte[32768];
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    outputStream.Write(buffer, 0, read);
+                }
+                outputStream.Close();
             }
             catch
             {
+                if (outputStream != null) outputStream.Close();
             }
-            return bitmap;
+        }
+
+        private object ExtractFromLocalStorage(Uri imageFileUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object LoadDefaultIfPassed(System.Uri imageFileUri, Object param2)
+        {
+            throw new NotImplementedException();
         }
 
         private Stream LoadFile(string file)
@@ -47,5 +130,8 @@ namespace FlickrPhoneApp
         {
             throw new NotImplementedException();
         }
+
+
+
     }
 }
